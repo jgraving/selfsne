7d311ca183cb4074ce226cae0327173837f17ddc
Jake <jgraving@gmail.com>
2022-05-02 10:56:31 +0200

    Update long description

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ import warnings
 from setuptools import setup, find_packages
 
 DESCRIPTION = "Self-Supervised Noise Embeddings (Self-SNE) for dimensionality reduction and clustering"
-LONG_DESCRIPTION = """
+LONG_DESCRIPTION = """Self-SNE is a probabilistic self-supervised deep learning model for compressing high-dimensional data to a low-dimensional embedding. It is a general-purpose algorithm that works with multiple types of data including images, sequences, and tabular data. It uses self-supervised objectives, such as InfoNCE, to preserve structure in the compressed latent space. Self-SNE can also (optionally) simultaneously learn a cluster distribution (a prior over the latent embedding) during optimization. Overlapping clusters are automatically combined by optimizing a variational upper bound on entropy, so the number of clusters does not have to be specified manually — provided the number of initial clusters is large enough. Self-SNE produces embeddings with similar quality to existing dimensionality reduction methods; can detect outliers; scales to large, out-of-core datasets; and can easily add new data to an existing embedding/clustering.
+
 """
 
 DISTNAME = "selfsne"

baab7c67f594ab13d9f7966c883bc8eb312d51e9
Jake <jgraving@gmail.com>
2022-05-03 17:15:16 +0200

    Update kernels

--- selfsne/kernels.py
+++ selfsne/kernels.py
@@ -28,6 +28,16 @@ class Laplace(Module):
         return -y.abs().sum(-1)
 
 
+class LaplaceT(Module):
+    def __init__(self, loc, scale=1.0):
+        super().__init__()
+        self.loc, self.scale = broadcast_all(loc, scale)
+
+    def log_prob(self, value):
+        y = (value - self.loc) / self.scale
+        return -y.abs().sum(-1).log1p()
+
+
 class StudentT(Module):
     def __init__(self, loc, scale=1.0):
         super().__init__()
@@ -51,22 +61,48 @@ class Normal(Module):
 class VonMises(Module):
     def __init__(self, loc):
         super().__init__()
-        self.concentration = loc
+        self.loc = loc / loc.norm(dim=-1, keepdim=True)
 
     def log_prob(self, value):
-        scale = self.concentration.norm(dim=-1)
-        log_normalizer = scale.log() - scale.sinh().log() - np.log(4 * np.pi)
-        return (self.concentration * value).sum(-1) + log_normalizer
+        value = value / value.norm(dim=-1, keepdim=True)
+        return (self.loc * value).sum(-1)
+
+
+class SphericalT(Module):
+    def __init__(self, loc):
+        super().__init__()
+        self.loc = loc / loc.norm(dim=-1, keepdim=True)
+
+    def log_prob(self, value):
+        value = value / value.norm(dim=-1, keepdim=True)
+        return -F.softplus(-(self.loc * value).sum(-1))
 
 
 class Categorical(Module):
     def __init__(self, logits):
-
-        self.logits = logits.log_softmax(-1)
         super().__init__()
+        self.logits = logits.log_softmax(-1)
 
     def log_prob(self, value):
         return (self.logits * value.softmax(-1)).sum(-1)
+
+
+class InnerProduct(Module):
+    def __init__(self, loc):
+        super().__init__()
+        self.loc = loc
+
+    def log_prob(self, value):
+        return (self.loc * value).sum(-1)
+
+
+class Bhattacharyya(Module):
+    def __init__(self, logits):
+        super().__init__()
+        self.logits = logits.log_softmax(-1)
+
+    def log_prob(self, value):
+        return 0.5 * (self.logits + value.log_softmax(-1)).logsumexp(-1)
 
 
 KERNELS = {
@@ -75,4 +111,8 @@ KERNELS = {
     "categorical": Categorical,
     "laplace": Laplace,
     "vonmises": VonMises,
+    "sphericalt": SphericalT,
+    "laplacet": LaplaceT,
+    "inner_product": InnerProduct,
+    "bhattacharyya": Bhattacharyya,
 }

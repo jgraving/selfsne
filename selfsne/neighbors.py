0a24b4fe5a2d017bb01a6cc54db59adf0b283f78
Jake <jgraving@gmail.com>
2023-01-26 12:20:47 +0100

    Update for full embeddings

--- selfsne/neighbors.py
+++ selfsne/neighbors.py
@@ -17,13 +17,44 @@ import torch
 import torch.nn.functional as F
 import torch.distributions as D
 from torch.nn import Module
+from torch.utils.data import Dataset
+
 import numpy as np
 
 
+class IndexedDataset(Dataset):
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __getitem__(self, index):
+        data = self.dataset[index]
+        return data, index
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+class IndexParser(Module):
+    def forward(self, batch):
+        if isinstance(batch, (tuple, list)) and len(batch) == 2:
+            _, index = batch
+        else:
+            raise InputError("index was not passed with batch.")
+        return index
+
+
 class Queue(Module):
-    def __init__(self, num_features, queue_size=2 ** 15, freeze_on_full=False):
+    def __init__(
+        self,
+        num_features,
+        queue_size=2 ** 15,
+        freeze_on_full=False,
+        dtype=torch.float32,
+    ):
         super(Queue, self).__init__()
-        self.register_buffer("queue", torch.zeros((queue_size, num_features)))
+        self.register_buffer(
+            "queue", torch.zeros((queue_size, num_features), dtype=dtype)
+        )
         self.max_size = queue_size
         self.queue_size = 0
         self.freeze_on_full = freeze_on_full
@@ -40,8 +71,10 @@ class Queue(Module):
     @torch.no_grad()
     def sample(self, n_samples):
         assert n_samples <= self.queue_size
-        indices = torch.randint(self.queue_size, (n_samples,), device=self.queue.device)
-        return self.queue[indices]
+        sample_idx = torch.randint(
+            self.queue_size, (n_samples,), device=self.queue.device
+        )
+        return self.queue[sample_idx]
 
     @property
     def full(self):
@@ -83,33 +116,58 @@ class NearestNeighborSampler(Module):
         metric="euclidean",
         perplexity=1,
         freeze_queue_on_full=False,
+        return_index=False,
     ):
         super().__init__()
-        self.queue = Queue(num_features, queue_size, freeze_queue_on_full)
+        self.data_queue = Queue(num_features, queue_size, freeze_queue_on_full)
         self.metric = METRICS[metric]
         self.perplexity = perplexity
+        self.return_index = return_index
+        if self.return_index:
+            self.index_queue = Queue(
+                1, queue_size, freeze_queue_on_full, dtype=torch.long
+            )
 
     @torch.no_grad()
-    def forward(self, x):
+    def forward(self, batch):
+        if isinstance(batch, (tuple, list)) and len(batch) == 2:
+            data, index = batch
+        else:
+            data = batch
+            if self.return_index:
+                raise InputError(
+                    "`return_index` is True, but index was not passed with batch."
+                )
+
         if self.training:
-            queue = self.queue(x)
-            distances = self.metric(x, queue)
-            if not self.queue.freeze:
+            if self.return_index:
+                index_queue = self.index_queue(index)
+
+            data_queue = self.data_queue(data)
+            distances = self.metric(data, data_queue)
+            if not self.data_queue.freeze:
                 # set self distances to inf
-                index = torch.arange(x.shape[0], device=x.device)
-                distances[index, index] = np.inf
-            perplexity = np.minimum(self.perplexity, queue.shape[0])
-            values, indices = torch.topk(distances, perplexity, dim=-1, largest=False)
+                batch_index = torch.arange(np.min(distances.shape), device=data.device)
+                distances[batch_index, batch_index] = np.inf
+
+            perplexity = np.minimum(self.perplexity, data_queue.shape[0])
+            _, knn_index = torch.topk(distances, perplexity, dim=-1, largest=False)
 
             if self.perplexity > 1:
-                idx = torch.arange(indices.shape[0], device=indices.device)
+                idx = torch.arange(knn_index.shape[0], device=knn_index.device)
                 jdx = torch.randint(
-                    0, perplexity, (indices.shape[0],), device=indices.device
+                    0, perplexity, (knn_index.shape[0],), device=knn_index.device
                 )
-                knn_idx = indices[idx, jdx]
+                neighbor_index = knn_index[idx, jdx]
             else:
-                knn_idx = indices[:, 0]
-            return queue[knn_idx]
+                neighbor_index = knn_index[:, 0]
 
+            if self.return_index:
+                return index_queue[neighbor_index]
+            else:
+                return data_queue[neighbor_index]
+
+        elif self.return_index:
+            return index
         else:
             return x

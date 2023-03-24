8f23af0aeb6f678cd07a3ba827b7e9a215afa261
Jake <jgraving@gmail.com>
2023-03-24 11:21:16 +0100

    Add docs

--- selfsne/neighbors.py
+++ selfsne/neighbors.py
@@ -16,41 +16,96 @@
 import torch
 import torch.nn.functional as F
 import torch.distributions as D
+
 from torch.nn import Module
 from torch.utils.data import Dataset
 
 from selfsne.kernels import PAIRWISE_KERNELS
 import numpy as np
 
+from typing import Tuple, List, Any, Union
+
 
 class IndexedDataset(Dataset):
-    def __init__(self, dataset):
+    """
+    Wraps an existing dataset and returns both the data and its index when __getitem__ is called.
+
+    Args:
+        dataset (Dataset): The dataset to wrap.
+    """
+
+    def __init__(self, dataset: Dataset):
         self.dataset = dataset
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> Tuple[Any, int]:
+        """
+        Get the data and its index at the specified index.
+
+        Args:
+            index (int): The index of the item to retrieve.
+
+        Returns:
+            Tuple[Any, int]: A tuple containing the data and its index.
+        """
         data = self.dataset[index]
         return data, index
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """
+        Get the length of the dataset.
+
+        Returns:
+            int: The length of the dataset.
+        """
         return len(self.dataset)
 
 
 class IndexParser(Module):
-    def forward(self, batch):
-        if isinstance(batch, (tuple, list)) and len(batch) == 2:
-            _, index = batch
-        else:
-            raise InputError("index was not passed with batch.")
+    """
+    Parses the index from a batch of data and returns it.
+
+    Args:
+        None
+    """
+
+    def forward(
+        self,
+        batch: Union[
+            Tuple[torch.Tensor, torch.Tensor], List[torch.Tensor, torch.Tensor]
+        ],
+    ) -> torch.Tensor:
+        """
+        Extracts the index from a batch of data.
+
+        Args:
+            batch (Union[Tuple[torch.Tensor, torch.Tensor], List[torch.Tensor, torch.Tensor]]): The batch of data to parse.
+                Should be a tuple or list containing two elements, where the first element is the
+                data and the second element is the index.
+
+        Returns:
+            torch.Tensor: The index extracted from the batch of data.
+        """
+        _, index = batch
         return index
 
 
 class Queue(Module):
+    """
+    A first-in-first-out (FIFO) queue.
+
+    Args:
+        num_features (int): The number of features in each data point that will be stored in the queue.
+        queue_size (int): The maximum size of the queue (default: 2 ** 15).
+        freeze_on_full (bool): Whether to stop updating the queue once it is full (default: False).
+        dtype (torch.dtype): The data type of the queue tensor (default: torch.float32).
+    """
+
     def __init__(
         self,
-        num_features,
-        queue_size=2 ** 15,
-        freeze_on_full=False,
-        dtype=torch.float32,
+        num_features: int,
+        queue_size: int = 2 ** 15,
+        freeze_on_full: bool = False,
+        dtype: torch.dtype = torch.float32,
     ):
         super(Queue, self).__init__()
         self.register_buffer(
@@ -61,7 +116,17 @@ class Queue(Module):
         self.freeze_on_full = freeze_on_full
 
     @torch.no_grad()
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Adds the given data to the queue buffer. If the buffer is full and `freeze_on_full` is True,
+        the buffer will not be updated.
+
+        Args:
+            x (torch.Tensor): The data to add to the buffer.
+
+        Returns:
+            torch.Tensor: The current state of the buffer.
+        """
         if self.freeze:
             return self.queue
         else:
@@ -70,7 +135,16 @@ class Queue(Module):
             return self.queue[: self.queue_size]
 
     @torch.no_grad()
-    def sample(self, n_samples):
+    def sample(self, n_samples: int) -> torch.Tensor:
+        """
+        Samples `n_samples` data points randomly from the buffer.
+
+        Args:
+            n_samples (int): The number of data points to sample from the buffer.
+
+        Returns:
+            torch.Tensor: A tensor containing the sampled data points.
+        """
         assert n_samples <= self.queue_size
         sample_idx = torch.randint(
             self.queue_size, (n_samples,), device=self.queue.device
@@ -78,51 +152,52 @@ class Queue(Module):
         return self.queue[sample_idx]
 
     @property
-    def full(self):
+    def full(self) -> bool:
+        """
+        Returns True if the buffer is full, otherwise False.
+
+        Returns:
+            bool: True if the buffer is full, otherwise False.
+        """
         return self.queue_size == self.max_size
 
     @property
-    def freeze(self):
+    def freeze(self) -> bool:
+        """
+        Returns True if the buffer is full and `freeze_on_full` is True, otherwise False.
+
+        Returns:
+            bool: True if the buffer is full and `freeze_on_full` is True, otherwise False.
+        """
         return self.full and self.freeze_on_full
 
 
-def inner_product(x, y):
-    return -(x @ y.T)
-
-
-def cross_entropy(x, y):
-    return inner_product(x.log().softmax(-1), y.log().log_softmax(-1))
-
-
-def cosine(x, y, eps=1e-8):
-    dot = inner_product(x, y)
-    x_norm = x.norm(dim=-1, keepdim=True)
-    y_norm = y.norm(dim=-1, keepdim=True)
-    return dot / (x_norm * y_norm.T + eps)
-
-
-METRICS = {
-    "euclidean": torch.cdist,
-    "inner_product": inner_product,
-    "cross_entropy": cross_entropy,
-    "cosine": cosine,
-}
-
-
 class NearestNeighborSampler(Module):
+    """
+    Performs nearest neighbor sampling on a given batch of data.
+
+    Args:
+        num_features (int): The number of features in each data point.
+        queue_size (int): The maximum size of the buffer for the data points (default: 2 ** 15).
+        kernel (str): The name of the kernel to use for calculating the distances (default: "euclidean").
+        num_neighbors (int): The number of nearest neighbors to sample from (default: 1).
+        freeze_queue_on_full (bool): Whether to stop updating the buffer once it is full (default: False).
+        return_index (bool): Whether to return the indices of the nearest neighbors (default: False).
+    """
+
     def __init__(
         self,
-        num_features,
-        queue_size=2 ** 15,
+        num_features: int,
+        queue_size: int = 2 ** 15,
         kernel: str = "euclidean",
-        perplexity=1,
-        freeze_queue_on_full=False,
-        return_index=False,
+        num_neighbors: int = 1,
+        freeze_queue_on_full: bool = False,
+        return_index: bool = False,
     ):
         super().__init__()
         self.data_queue = Queue(num_features, queue_size, freeze_queue_on_full)
         self.kernel = PAIRWISE_KERNELS[kernel]
-        self.perplexity = perplexity
+        self.num_neighbors = num_neighbors
         self.return_index = return_index
         if self.return_index:
             self.index_queue = Queue(
@@ -130,13 +205,30 @@ class NearestNeighborSampler(Module):
             )
 
     @torch.no_grad()
-    def forward(self, batch):
+    def forward(
+        self,
+        batch: Union[
+            torch.Tensor,
+            Tuple[torch.Tensor, torch.Tensor],
+            List[torch.Tensor, torch.Tensor],
+        ],
+    ) -> torch.Tensor:
+        """
+        Returns the nearest neighbors to the input data.
+
+        Args:
+            batch (Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor], List[torch.Tensor, torch.Tensor]])): The batch of data to sample from.
+                Should be a tensor or a tuple of two tensors containing the data and their indices.
+
+        Returns:
+            torch.Tensor: A tensor containing the nearest neighbor samples.
+        """
         if isinstance(batch, (tuple, list)) and len(batch) == 2:
             data, index = batch
         else:
             data = batch
             if self.return_index:
-                raise InputError(
+                warnings.warn(
                     "`return_index` is True, but index was not passed with batch."
                 )
 
@@ -151,13 +243,13 @@ class NearestNeighborSampler(Module):
                 batch_index = torch.arange(np.min(distances.shape), device=data.device)
                 distances[batch_index, batch_index] = np.inf
 
-            perplexity = np.minimum(self.perplexity, data_queue.shape[0])
+            num_neighbors = np.minimum(self.num_neighbors, data_queue.shape[0])
             _, knn_index = torch.topk(distances, num_neighbors, dim=-1)
 
-            if self.perplexity > 1:
+            if self.num_neighbors > 1:
                 idx = torch.arange(knn_index.shape[0], device=knn_index.device)
                 jdx = torch.randint(
-                    0, perplexity, (knn_index.shape[0],), device=knn_index.device
+                    0, num_neighbors, (knn_index.shape[0],), device=knn_index.device
                 )
                 neighbor_index = knn_index[idx, jdx]
             else:
@@ -172,3 +264,19 @@ class NearestNeighborSampler(Module):
             return index
         else:
             return x
+
+
+def random_sample_columns(x: torch.Tensor, num_samples: int) -> torch.Tensor:
+    """
+    Randomly samples columns from the input tensor.
+
+    Args:
+        x (torch.Tensor): The input tensor to sample from.
+        num_samples (int): The number of columns to sample.
+
+    Returns:
+        torch.Tensor: A tensor containing the sampled columns.
+    """
+    idx = torch.arange(x.shape[0], device=x.device)
+    jdx = torch.randint(0, num_samples, (x.shape[0],), device=x.device)
+    return x[idx, jdx]

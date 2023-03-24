e8fe4e721102ab6445915ac525aed5babc106947
Jake <jgraving@gmail.com>
2023-03-24 17:38:57 +0100

    Update docs

--- selfsne/losses.py
+++ selfsne/losses.py
@@ -57,96 +57,80 @@ from selfsne.utils import (
     random_sample_columns,
 )
 
+from typing import Optional, Union, Tuple
+
 
 class DensityRatioEstimator(nn.Module):
     """
     A generalized multi-sample contrastive loss based on
-    Noise Contrastive Estimation (NCE) [1] and its variants [2, 3]
-    that preserves local embedding structure by maximizing similarity
-    for positive pairs and minimizing similarity for negative (noise) pairs.
+    Noise Contrastive Estimation (NCE) that preserves local embedding
+    structure by maximizing similarity for positive pairs and minimizing similarity
+    for negative (noise) pairs.
 
     A user-selected similarity kernel is used to calculate the log density ratio ("logits")
-    for each pair, which are then optimized by a user-selected divergence function
-    to maximize logits for positive pairs (attraction)
-    and minimize logits for negative pairs (repulsion).
+    for each pair, which are then optimized by a user-selected divergence function to
+    maximize logits for positive pairs (attraction) and minimize logits for negative pairs
+    (repulsion).
 
-    Parameters
-    ----------
-    kernel: str
-        Similarity kernel used for calculating logits.
-        Must be one of selfsne.kernels.KERNELS.
-        For example, "cauchy" can be used to produce t-SNE [4] or UMAP [5]
-        embeddings, "normal" can be used to produce SNE [6] embeddings,
-        and "vonmises" can be used for (hyper)spherical embeddings [7, 8].
+    Args:
+        kernel (Union[str, callable]): Similarity kernel used for calculating logits.
+            Must be one of selfsne.kernels.KERNELS or a callable that takes in two 2D tensors and returns a 2D tensor.
+            For example, "cauchy" can be used to produce t-SNE or UMAP embeddings, "normal" can be used to
+            produce SNE embeddings, and "vonmises" can be used for (hyper)spherical embeddings.
+        kernel_scale (Union[float, str]): Positive scale value for calculating logits.
+            For loc-scale family kernels sqrt(embedding_dims) is recommended,
+            which is calculated automatically when kernel_scale = "auto". Default is 1.0.
+        divergence (Union[str, callable]): Divergence function used for instance classification.
+            Must be one of selfsne.divergences.DIVERGENCES or a callable that takes in two 2D tensors and returns a scalar.
+        baseline (Union[str, float, callable]): The baseline for calculating the log density ratio.
+            Must be a float, string (one of selfsne.baselines.BASELINES), or nn.Module such as from selfsne.baselines.
+            Default is 0.
+        num_negatives (Optional[int]): Number of negative samples to use. Default is None.
 
-    kernel_scale: float or str, default=1.0
-        Postive scale value for calculating logits.
-        For loc-scale family kernels sqrt(embedding_dims) is recommended,
-        which is calculated automatically when kernel_scale = "auto".
+    Returns:
+        Tuple[torch.Tensor]: A tuple containing four tensors:
+            [0] The mean of the positive logits, i.e., the diagonal entries of the
+                kernel matrix (shape: (1,))
+            [1] The mean of the negative logits, i.e., the off-diagonal entries of
+                the kernel matrix (shape: (1,))
+            [2] The mean of the log-baseline (shape: (1,))
+            [3] The similarity loss, the sum of the mean attraction and mean repulsion terms (shape: (1,))
 
-    divergence: str
-        Divergence function used for instance classification.
-        Must be one of selfsne.divergences.DIVERGENCES.
-
-    baseline : float, str, or nn.Module, default = 0
-        The baseline for calculating the log density ratio.
-        Must be a float, string (one of selfsne.baselines.BASELINES),
-        or nn.Module such as from selfsne.baselines
-
-    attraction_weight: float, default=1.0
-        Weighting for the attraction term
-
-    repulsion_weight: float, default=1.0
-        Weighting for the repulsion term
-
-    baseline_weight: float, default=1.0
-        Weighting for the baseline term,
-        where log_baseline += log(baseline_weight)
-
-    References
-    ----------
-    [1] Gutmann, M., & Hyvärinen, A. (2010). Noise-contrastive estimation:
-        A new estimation principle for unnormalized statistical models.
-        In Proceedings of the thirteenth international conference on artificial
-        intelligence and statistics (pp. 297-304). JMLR Workshop and
-        Conference Proceedings.
-
-    [2] Oord, A. V. D., Li, Y., & Vinyals, O. (2018).
-        Representation learning with contrastive predictive coding.
-        arXiv preprint arXiv:1807.03748.
-
-    [3] Van Der Maaten, L. (2009). Learning a parametric embedding
-        by preserving local structure. In Artificial intelligence
-        and statistics (pp. 384-391). PMLR.
-
-    [4] Sainburg, T., McInnes, L., & Gentner, T. Q. (2021).
-        Parametric UMAP Embeddings for Representation and Semisupervised
-        Learning. Neural Computation, 33(11), 2881-2907.
-
-    [5] Hinton, G. E., & Roweis, S. (2002). Stochastic neighbor embedding.
-        Advances in neural information processing systems, 15.
-
-    [6] Chen, T., Kornblith, S., Norouzi, M., & Hinton, G. (2020).
-        A simple framework for contrastive learning of visual representations.
-        In International conference on machine learning (pp. 1597-1607). PMLR.
-
-    [7] Wang, M., & Wang, D. (2016). Vmf-sne: Embedding for spherical
-        data. In 2016 IEEE International Conference on Acoustics, Speech
-        and Signal Processing (ICASSP) (pp. 2344-2348). IEEE.
+    References:
+        [1] Gutmann, M., & Hyvärinen, A. (2010). Noise-contrastive estimation:
+            A new estimation principle for unnormalized statistical models.
+            In Proceedings of the thirteenth international conference on artificial
+            intelligence and statistics (pp. 297-304). JMLR Workshop and
+            Conference Proceedings.
+        [2] Oord, A. V. D., Li, Y., & Vinyals, O. (2018).
+            Representation learning with contrastive predictive coding.
+            arXiv preprint arXiv:1807.03748.
+        [3] Van Der Maaten, L. (2009). Learning a parametric embedding
+            by preserving local structure. In Artificial intelligence
+            and statistics (pp. 384-391). PMLR.
+        [4] Sainburg, T., McInnes, L., & Gentner, T. Q. (2021).
+            Parametric UMAP Embeddings for Representation and Semisupervised
+            Learning. Neural Computation, 33(11), 2881-2907.
+        [5] Hinton, G. E., & Roweis, S. (2002). Stochastic neighbor embedding.
+            Advances in neural information processing systems, 15.
+        [6] Chen, T., Kornblith, S., Norouzi, M., & Hinton, G. (2020).
+            A simple framework for contrastive learning of visual representations.
+            In International conference on machine learning (pp. 1597-1607). PMLR.
+        [7] Wang, M., & Wang, D. (2016). Vmf-sne: Embedding for spherical
+            data. In 2016 IEEE International Conference on Acoustics, Speech
+            and Signal Processing (ICASSP) (pp. 2344-2348). IEEE.
 
     """
 
     def __init__(
         self,
-        kernel="cauchy",
-        kernel_scale=1.0,
-        divergence="kld",
-        baseline=0,
-        attraction_weight=1.0,
-        repulsion_weight=1.0,
-        baseline_weight=1.0,
-        num_negatives=None,
-    ):
+        kernel: Union[str, callable] = "cauchy",
+        kernel_scale: Union[float, str] = 1.0,
+        divergence: Union[str, callable] = "kld",
+        baseline: Union[str, float, callable] = 0,
+        num_negatives: Optional[int] = None,
+    ) -> None:
+
         super().__init__()
         if isinstance(kernel, str):
             self.kernel = PAIRWISE_KERNELS[kernel]
@@ -167,143 +151,166 @@ class DensityRatioEstimator(nn.Module):
         else:
             self.baseline = baseline
 
-        self.attraction_weight = attraction_weight
-        self.repulsion_weight = repulsion_weight
-        self.log_baseline_weight = np.log(baseline_weight)
         self.num_negatives = num_negatives
 
-    def forward(self, z_x, z_y, y):
+    def forward(
+        self, z_x: torch.Tensor, z_y: torch.Tensor, y: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor]:
+        """
+        Computes similarity loss.
+
+        Args:
+            z_x (torch.Tensor): Input tensor with shape (batch_size, embedding_features)
+                representing the features of the x domain.
+            z_y (torch.Tensor): Input tensor with shape (batch_size, embedding_features)
+                representing the embedding of the y labels.
+            y (torch.Tensor, optional): Input tensor with shape (batch_size, **data_features) representing
+                the original y data. Used when the baseline is a LearnedConditionalBaseline.
+                Default: None.
+
+        Returns:
+            Tuple[torch.Tensor]: A tuple containing four tensors:
+                [0] The mean of the positive logits, i.e., the diagonal entries of the
+                    kernel matrix (shape: (1,))
+                [1] The mean of the negative logits, i.e., the off-diagonal entries of
+                    the kernel matrix (shape: (1,))
+                [2] The mean of the log-baseline (shape: (1,))
+                [3] The similarity loss, the sum of the mean attraction and mean repulsion terms (shape: (1,))
+        """
         if self.kernel_scale == "auto":
-            self.kernel_scale = np.sqrt(z_x.shape[1])
+            embedding_features = z_x.shape[1]
+            self.kernel_scale = np.sqrt(embedding_features)
 
         logits = self.kernel(z_y, z_x, self.kernel_scale)
-        log_baseline = self.baseline(y, logits)
-        logits = logits - log_baseline - self.log_baseline_weight
+        log_baseline = self.baseline(logits, y)
+        logits = logits - log_baseline
         pos_logits = diagonal(logits).unsqueeze(1)
         neg_logits = remove_diagonal(logits)
         if self.num_negatives:
             neg_logits = random_sample_columns(neg_logits, self.num_negatives)
         attraction, repulsion = self.divergence(pos_logits, neg_logits)
         return (
-            pos_logits.mean() + self.log_baseline_weight,
-            neg_logits.mean() + self.log_baseline_weight,
+            pos_logits.mean(),
+            neg_logits.mean(),
             log_baseline.mean(),
-            attraction.mean() * self.attraction_weight
-            + repulsion.mean() * self.repulsion_weight,
+            attraction.mean() + repulsion.mean(),
         )
 
 
-def random_sample_columns(x, num_samples):
+def random_sample_columns(x: torch.Tensor, num_samples: int) -> torch.Tensor:
+    """
+    Randomly samples columns from the input tensor.
+
+    Args:
+        x (torch.Tensor): The input tensor to sample from.
+        num_samples (int): The number of columns to sample.
+
+    Returns:
+        torch.Tensor: A tensor of shape (x.shape[0], num_samples), where each row of the output
+            tensor is a random selection of columns from the corresponding row of the input tensor.
+
+    Example:
+        >>> x = torch.randn((4, 6))
+        >>> random_sample_columns(x, 3)
+        tensor([[ 0.6305,  0.1669, -1.2118],
+                [ 0.5246, -0.0237, -1.2891],
+                [-1.3693, -0.1853, -0.3473],
+                [ 0.6842,  1.5442,  0.5549]])
+    """
     idx = torch.arange(x.shape[0], device=x.device).unsqueeze(1).repeat(1, num_samples)
     jdx = torch.randint(0, x.shape[1], (x.shape[0], num_samples), device=x.device)
     return x[idx, jdx]
 
 
 class RedundancyReduction(nn.Module):
-    """Redundancy Reduction [1]
+    """
+    Redundancy Reduction loss function [1] that creates an embedding by minimizing
+    mean squared error between an identity matrix and the empirical cross-correlation matrix
+    between positive pairs. This helps to maximize feature invariance to differences between
+    positive pairs while minimizing redundancy (correlation) between features, preserving global
+    structure in the embedding as a form of nonlinear Canonical Correlation Analysis (CCA) [2].
 
-    A self-supervised loss that creates an an embedding by minimizing
-    mean squared error between an identity matrix and the
-    empirical cross-correlation matrix between positive pairs,
-    which maximizes feature invariance to differences between positive pairs
-    while also minimizing redundancy (correlation) between features.
-    This helps to preserve global structure in the embedding as a form of
-    nonlinear Canonical Correlation Analysis (CCA) [2].
+    Args:
+        num_features (int): Number of embedding features.
+        invariance_weight (float, optional): Weighting for the invariance term in the loss.
+            Default: 1.0.
+        redundancy_weight (float, optional): Weighting for the redundancy term in the loss.
+            Default: 1.0.
 
-    Parameters
-    ----------
-    num_features: int
-        Number of embedding features
+    Returns:
+        loss (torch.Tensor): The computed redundancy reduction loss.
 
-    invariance_weight: float, default=1.0
-        Weighting for the invariance term
-
-    redundancy_weight: float, default=1.0
-        Weighting for the redundancy term
-
-
-    References
-    ----------
-    [1] Zbontar, J., Jing, L., Misra, I., LeCun, Y., & Deny, S. (2021).
+    References:
+        [1] Zbontar, J., Jing, L., Misra, I., LeCun, Y., & Deny, S. (2021).
         Barlow Twins: Self-Supervised Learning via Redundancy Reduction.
         In International Conference on Machine Learning (pp. 12310-12320).
         PMLR.
-
-    [2] Balestriero, R., & LeCun, Y. (2022). Contrastive and Non-Contrastive
+        [2] Balestriero, R., & LeCun, Y. (2022). Contrastive and Non-Contrastive
         Self-Supervised Learning Recover Global and Local Spectral Embedding
         Methods. doi:10.48550/arxiv.2205.11508
-
     """
 
-    def __init__(self, num_features=2, invariance_weight=1.0, redundancy_weight=1.0):
+    def __init__(
+        self,
+        num_features: int = 2,
+        invariance_weight: float = 1.0,
+        redundancy_weight: float = 1.0,
+    ) -> None:
         super().__init__()
-        self.baseline = nn.BatchNorm1d(num_features, affine=False)
+        self.norm = nn.BatchNorm1d(num_features, affine=False)
         self.invariance_weight = invariance_weight
         self.redundancy_weight = redundancy_weight
 
-    def forward(self, x, y):
-        n, d = x.shape
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        batch_size, _ = x.shape
 
-        correlation = self.baseline(x).T @ self.baseline(y) / n
-        invariance = diagonal(correlation).add_(-1).pow_(2).mean()  # .sum() / d
-        redundancy = off_diagonal(correlation).pow_(2).mean()  # .sum() / (d * d - d)
+        correlation = self.norm(x).T @ self.norm(y) / batch_size
+        invariance = diagonal(correlation).add_(-1).pow_(2).mean()
+        redundancy = off_diagonal(correlation).pow_(2).mean()
 
         return invariance * self.invariance_weight + redundancy * self.redundancy_weight
 
 
 class VICReg(nn.Module):
-    """Variance-Invariance-Covariance Regularization [1]
-
-    A self-supervised embedding loss that combinines three terms:
+    """
+    Variance-Invariance-Covariance (VIC) regularization loss function [1] that creates an
+    embedding by combining three terms:
     (1) a variance stabilizing term (hinge loss for feature-wise std. dev.),
     (2) an invariance term (mean squared error between positive pairs),
-    (3) a covariance regularization (minimize squared covariance), which is
-    a decorrelation mechanism based on redundancy reduction [2].
-    This helps to preserve global structure in the embedding as a form of
-    Laplacian Eigenmaps [3].
+    (3) a covariance regularization term (minimize squared covariance) based on redundancy reduction [2].
+    This helps to preserve global structure in the embedding as a form of Laplacian Eigenmaps [3].
 
-    Parameters
-    ----------
-    num_features: int
-        Number of embedding features
+    Args:
+        num_features (int): Number of embedding features.
+        eps (float, optional): A value added to the variance term for numerical stability. Default: 1e-8.
+        variance_weight (float, optional): Weighting for the variance term in the loss. Default: 1.0.
+        invariance_weight (float, optional): Weighting for the invariance term in the loss. Default: 1.0.
+        covariance_weight (float, optional): Weighting for the covariance term in the loss. Default: 1.0.
 
-    eps: float, default=1e-8
-        A value added to the variance term for numerical stability
+    Returns:
+        loss (torch.Tensor): The computed loss.
 
-    variance_weight: float, default=1.0
-        Weighting for the variance term
-
-    invariance_weight: float, default=1.0
-        Weighting for the invariance term
-
-    covariance_weight: float, default=1.0
-        Weighting for the covariance term
-
-    References
-    ----------
-    [1] Bardes, A., Ponce, J., & LeCun, Y. (2021). VICReg:
+    References:
+        [1] Bardes, A., Ponce, J., & LeCun, Y. (2021). VICReg:
         Variance-Invariance-Covariance Regularization for self-supervised
         learning. arXiv preprint arXiv:2105.04906.
-
-    [1] Zbontar, J., Jing, L., Misra, I., LeCun, Y., & Deny, S. (2021).
+        [2] Zbontar, J., Jing, L., Misra, I., LeCun, Y., & Deny, S. (2021).
         Barlow Twins: Self-Supervised Learning via Redundancy Reduction.
         In International Conference on Machine Learning (pp. 12310-12320).
         PMLR.
-
-    [2] Balestriero, R., & LeCun, Y. (2022). Contrastive and Non-Contrastive
+        [3] Balestriero, R., & LeCun, Y. (2022). Contrastive and Non-Contrastive
         Self-Supervised Learning Recover Global and Local Spectral Embedding
         Methods. doi:10.48550/arxiv.2205.11508
-
     """
 
     def __init__(
         self,
-        num_features,
-        eps=1e-8,
-        variance_weight=1.0,
-        invariance_weight=1.0,
-        covariance_weight=1.0,
-    ):
+        num_features: int,
+        eps: float = 1e-8,
+        variance_weight: float = 1.0,
+        invariance_weight: float = 1.0,
+        covariance_weight: float = 1.0,
+    ) -> None:
         super().__init__()
         self.num_features = num_features
         self.eps = eps
@@ -311,22 +318,21 @@ class VICReg(nn.Module):
         self.invariance_weight = invariance_weight
         self.covariance_weight = covariance_weight
 
-    def forward(self, x, y):
-
-        n, d = x.shape
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        batch_size, embedding_features = x.shape
         invariance = F.mse_loss(x, y)
 
         x = x - x.mean(dim=0)
         y = y - y.mean(dim=0)
 
-        cov_x = (x.T @ x) / (n - 1)
-        cov_y = (y.T @ y) / (n - 1)
+        cov_x = (x.T @ x) / (batch_size - 1)
+        cov_y = (y.T @ y) / (batch_size - 1)
         std_x = diagonal(cov_x).add(self.eps).sqrt()
         std_y = diagonal(cov_y).add(self.eps).sqrt()
         variance = F.relu(1 - std_x).mean() / 2 + F.relu(1 - std_y).mean() / 2
         covariance = (
-            off_diagonal(cov_x).pow_(2).sum().div(d) / 2
-            + off_diagonal(cov_y).pow_(2).sum().div(d) / 2
+            off_diagonal(cov_x).pow_(2).sum().div(embedding_features) / 2
+            + off_diagonal(cov_y).pow_(2).sum().div(embedding_features) / 2
         )
 
         return (
